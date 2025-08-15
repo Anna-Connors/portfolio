a6ff5f528c60091e339301a41b0881a1767a8cb1
Anna-Connors <aconnors281@gmail.com>
2025-08-15 10:53:26 -0500

    under construction added to contact & project

--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -2,12 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const ProjectsPage = () => (
-  <Layout>
-    <h1>Projects</h1>
-    <p>Coming soon!</p>
-  </Layout>
-)
+const ProjectsPage = () => {
+
+  return (
+    <Layout>
+      <h1>Projects</h1>
+      <p>Coming soon!</p>
+    </Layout>
+  )
+
+}
 
 export const Head = () => <Seo title="Projects" />
 export default ProjectsPage
